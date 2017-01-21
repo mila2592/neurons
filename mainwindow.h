3de43c731085bc3324964aa127c3914bec8a55cf
mila2592 <arksinus-bmw@yandex.ru>
2017-01-21 23:24:34 +0300

    mainwindow

--- mainwindow.h
+++ mainwindow.h
@@ -121,7 +121,7 @@ public:
     QImage *img,*img1;
 
     QPushButton *conns_btn,*start_btn,*stop_btn,*newLay_btn;
-    QLabel *label1,*label2,*time_lbl;
+    QLabel *label1,*label2,*time_lbl,*max_time_lbl;
 
     QwtPlot *plot1,*plot2;
     QwtPlotCurve *curve1;
@@ -130,7 +130,7 @@ public:
     QPainter *p1;
 
     QLCDNumber *time_lcd;
-    QSpinBox *max_time;
+ QSpinBox *max_time;
     void paintEvent(QPaintEvent* );
 private slots:
     void drawFiringNeur();
@@ -138,7 +138,7 @@ private slots:
     void startIntegrate();
     void stopIntegrate();
     void newLay();
-    void setMaxTime();
+        void setMaxTime();
 };
 
 #endif // MAINWINDOW_H
