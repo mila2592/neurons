3ecfa0a63cf12ec519db47bbf87bc74fd40684fc
mila2592 <arksinus-bmw@yandex.ru>
2017-02-15 13:24:03 +0300

    main

--- main.cpp
+++ main.cpp
@@ -1,19 +1,27 @@
 ﻿#include <QtGui/QApplication>
 #include <QTextCodec>
-#include "core.h"
+//#include "core.h"
+#include "netmap.h"
 //#include "neuronmainwidget.h"
-
+#include "mainwindow.h"
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
     QTextCodec::setCodecForTr(QTextCodec::codecForName ("Windows-1251"));
 
-    Core::instance().init_core();
-    //NeuronMainWidget main_widget;
-    //main_widget.show();
-    //neur wnd;
-    //wnd.show();
+        NetMap wnd;
+        wnd.show();
 
+//    Core::instance().init_core();
+
+//////////////////////////////////
+//    NeuronMainWidget main_widget;
+//    main_widget.show();
+
+////////////////////////
+//    neur wnd;
+//    wnd.show();
+///////////////////////
     return a.exec();
 }
